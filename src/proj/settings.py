b8b26ccb8f79fbd518292b9a5ee7b4a0b7c2dc6d
ruslanshkred <r.shkred@gmail.com>
2022-10-05 13:16:55 +0300

    234

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-from . import local_settings
+import local_settings
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
